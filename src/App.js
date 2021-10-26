1bfd569970d4fba65a15febf098c8f80ad17e8c7
Miguel Fernandez <miguelfernandez@Miguels-Mac-mini.local>
2021-10-26 19:19:23 -0400

    fixed path for home page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Route exact path="/home" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/contact" component={Contact} />
